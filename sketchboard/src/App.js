62d687c3feacbb0d84b21e1450bc3d96477bf44f
segalzeyalz <segaleseo@gmail.com>
2018-11-20 11:42:08 +0200

    add to design

--- sketchboard/src/App.js
+++ sketchboard/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Buttons from './buttons/Buttons';
+
 import './App.css';
 
 class App extends Component {
@@ -7,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <Buttons />
       </div>
     );
   }
---
+++ sketchboard/src/buttons/Buttons.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Buttons extends Component {
+  render() {
+      //Here is the header
+    return (
+      <div className="buttons">
+        <div className="button">Sign In</div>
+        <div className="button">Sign Up</div>
+        <div className="button">Save</div>
+        <div className="button">Load</div>
+        <div className="button">Color</div>
+        <div className="button">Picture</div>
+        <div className="button">Oval</div>
+        <div className="button">Triangle</div>
+        <div className="button">Rectangle</div>
+      </div>
+    );
+  }
+}
+
+export default Buttons;
