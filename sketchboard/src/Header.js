8b171e75031c34014c6419c1edb62faa1e7938b8
segalzeyalz <segaleseo@gmail.com>
2018-11-20 07:24:41 +0200

    making the design of code

---
+++ sketchboard/src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+      //Here is the header
+    return (
+      <div className="App">
+      <h1>SketchBoard</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
