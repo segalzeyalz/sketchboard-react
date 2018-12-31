6a3407f356262e81d6d1168a5afe45bf60bf1ca2
segalzeyalz <segaleseo@gmail.com>
2018-12-31 11:31:59 +0200

    adding posX and posY functionality

--- sketchboard/src/containers/canvas/Canvas.js
+++ sketchboard/src/containers/canvas/Canvas.js
@@ -6,11 +6,16 @@ class Canvas extends Component {
     return (
       <div className="canvas">
         {this.props.shapes.map(function(shape){
+          console.log(shape.shape)
           var style = {
             "border":"1px solid black",
             "width":shape.shape.width,
             "height": shape.shape.height,
-            "background-color": shape.color
+            "background-color": shape.color,
+            "top":shape.shape.posY,
+            "left":shape.shape.posX,
+            "position": "absolute"
+            
           }
           if(shape.shape.shapeName=="Oval"){
             style= {...style, "border-radius":shape.shape.borderRadius}
